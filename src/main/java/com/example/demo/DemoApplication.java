4339dedf22042542c166f732ad6d866ab6185921
Muqthar <muqthar_ali@yahoo.com>
2018-10-02 22:39:48 +0530

    Updated application java file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
 		System.out.println("GIT Feature2");
+
+		System.out.println("Feature 2");
+
 	}
 }
