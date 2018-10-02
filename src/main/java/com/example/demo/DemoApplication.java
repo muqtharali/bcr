d8c30a86b05b063bcf80dbecf6e600683df89ddf
Muqthar <muqthar_ali@yahoo.com>
2018-10-02 22:46:02 +0530

    Updated applicatio java file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 @SpringBootApplication
 public class DemoApplication {
@@ -7,7 +8,22 @@ public class DemoApplication {
 
 		System.out.println("GIT Feature2");
 
-		System.out.println("Old Feature 2");
+		System.out.println("Latest Feature 2");
 
 	}
 }
+=======
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println("GIT Feature2");
+
+		System.out.println("Latest Feature 2");
+
+	}
+}
+>>>>>>> Updated applicatio java file
